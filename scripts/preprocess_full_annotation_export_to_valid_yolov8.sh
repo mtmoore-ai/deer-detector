2aee32604357001016662af8d9c78fc95cf2e271
mtmoore-ai <mtmoore@gmail.com>
2024-11-14 04:55:21 -0600

    handle pre-extracted dataset

--- scripts/preprocess_full_annotation_export_to_valid_yolov8.sh
+++ scripts/preprocess_full_annotation_export_to_valid_yolov8.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-orig_img_location="/data/mtmoore/school/CSiML_AI395T/final_project/dataset/build/validation/deer/"
-orig_label_dir="labels/train/cuts/deer"
+if [ "$#" -ne "2" ]; then
+    echo "usage: $0 full-path-to-all-images relative-path-in-zip-to-label-txts"
+    echo "$# arguments passed instead of 2"
+    exit
+fi
+
+orig_img_location="$1"
+orig_label_dir="$2"
+
+# if the dataset contents are already in the directory
+# don't remove labels and images
+DATASET_EXTRACTED=1
 
 # cleanup previous contents
-rm -rf labels train val 2>/dev/null
-rm data.yaml train.txt val.txt
+if [ "${DATASET_EXTRACTED}" -lt "1" ]; then
+    rm -rf data.yaml labels train val 2>/dev/null
+fi
+rm -rf train val train.txt val.txt 2>/dev/null
 
-unzip -qq yolov8_detection.zip
+# if dataset has images and labels don't unzip 
+if [ "${DATASET_EXTRACTED}" -lt "1" ]; then
+    unzip -qq ../yolov8_detection.zip
+fi
 
 mkdir -p {train,val}/{images,labels}
 
@@ -17,6 +32,7 @@ shuf tmp.labels > tmp.shuf.labels
 all_count=$(cat tmp.shuf.labels | wc -l)
 train_count=$(echo "($all_count * 0.8)/1" | bc)
 val_count=$((all_count - train_count))
+echo "${all_count} labels, ${train_count} to train, ${val_count} to val"
 
 head -n ${train_count} tmp.shuf.labels > train.txt
 tail -n ${val_count} tmp.shuf.labels > val.txt
@@ -32,6 +48,10 @@ for split in "train" "val"; do
     sed -i "s|^|${split}/images/|" ${split}.txt
 done
 
-
 echo "val: val.txt" >> data.yaml
-sed -i "s|path: .
+sed -i "s|path: .|path: $(pwd)|" data.yaml
+
+# if dataset had images now remove them
+#if [ "${DATASET_EXTRACTED}" -lt "1" ]; then
+#    rm -rf images labels 2>/dev/null
+#fi
